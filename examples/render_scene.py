a82e38559430f2f7b5d89233f55237607d58ab79
XuPaya <50351365+XuPaya@users.noreply.github.com>
2026-05-12 23:01:43 -0500

    wrap-up: experiments and readme

---
+++ examples/render_scene.py
@@ -0,0 +1,38 @@
+import time
+import mitsuba as mi
+import drjit as dr
+import numpy as np
+from tqdm import tqdm
+
+mi.set_variant('cuda_ad_spectral')
+
+import mitsuba_realistic_camera  # noqa: F401
+
+start = time.time()
+scene = mi.load_file(
+    './scenes/kitchen/scene_realistic_fisheye.xml'
+)
+
+n_trials = 16
+
+image = None
+dr.disable_grad() 
+for i in tqdm(range(n_trials)):
+    image_i = mi.render(scene, spp=128, seed=i)
+    if image is None:
+        image = image_i
+    else:
+        image += image_i
+image /= n_trials
+out = './kitchen_dgauss.exr'
+mi.Bitmap(image).write(out)
+
+arr = np.array(image)
+print('wrote', out)
+print(
+    'shape', arr.shape,
+    'mean', float(arr.mean()),
+    'max', float(arr.max()),
+    'nonzero', int(np.count_nonzero(arr)),
+    'seconds', time.time() - start,
+)
---
+++ mitsuba_realistic_camera/__init__.py
@@ -0,0 +1,3 @@
+from .realistic_camera import RealisticCamera, register_realistic_camera
+
+__all__ = ["RealisticCamera", "register_realistic_camera"]
